168f516e711f26084b644222ed265b3b3307816c
Nandan Sridhar <srinandans@srinandans-macbookpro.roam.corp.google.com>
2017-07-26 14:52:01 -0400

    code updates

--- apiproxy/resources/node/index.js
+++ apiproxy/resources/node/index.js
@@ -30,20 +30,56 @@
      "error": "invalid_request",
      "error_description": "invalid request"
  };
-
+ 
 var success = {
     "message": "assertion is valid"
 }
 
+function getPublicKey(publickeys, kid) {
+    var key = "";
+    if (publickeys.length == 1) return publickeys[0];
+	for (i =0; i < publickeys.length; i++ ) {
+		var t = publickeys[i];
+		if(t[0] == kid) {
+			key = t[1];
+			break;
+		}
+	}
+	return key;	
+}
+
+
  var server = http.createServer(function(request, response) {
 
-     var publicKey = apigee.getVariable(request, "private.publicKey");
+     var publicKeyResponse = apigee.getVariable(request, "publicKey");
+     if (!publicKeyResponse) {
+         publicKeyResponse = apigee.getVariable(request, "publicKeyResponse.content");
+     }
+     
+     var publicKeyResponseObj = JSON.parse(publicKeyResponse);
      var assertion = apigee.getVariable(request,"assertion");
 
      try {
-      console.log(request.url);
          if (request.url == "/token" && request.method == "POST") {
-             var result = jws.verify(assertion, "RS256", publicKey); 
+             var decode = jws.decode(assertion);
+             if (!decode || !decode.header || !decode.header.kid) {
+                 response.writeHead(500, {
+                     "Content-Type": "application/json"
+                 });
+                 response.end(JSON.stringify(err));                 
+             }
+             var publickeys = publicKeyResponseObj.keys;
+             var publicKey = getPublicKey(publickeys, decode.header.kid);
+             if (!publicKey) {
+                 response.writeHead(500, {
+                     "Content-Type": "application/json"
+                 });
+                 response.end(JSON.stringify(err));                 
+             }
+             var key = rs.KEYUTIL.getKey(publicKey);
+             var pem = rs.KEYUTIL.getPEM(key);
+             //var result = rs.jws.JWS.verify(assertion, publicKey, ['RS256']);//
+             var result = jws.verify(assertion, "RS256", pem); 
              if (result) {
               response.writeHead(200, {
                   "Content-Type": "application/json"
